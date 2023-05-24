3221b66e82b868106fa98b5f72eb02d9110e2b7a
gaelchevalier <89252419+gaelchevalier@users.noreply.github.com>
2023-05-24 17:07:17 +0200

    Demo utiliser le model

--- src/main/java/fr/example/demo/controller/HomeController.java
+++ src/main/java/fr/example/demo/controller/HomeController.java
@@ -2,7 +2,10 @@ package fr.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.example.demo.injection.ServicePerson;
 
@@ -19,5 +22,34 @@ public class HomeController {
 		servicePerson.test();
 		
 		return "home";
+	}
+	
+	@GetMapping("/article/{id}")
+	public String article(@PathVariable("id") int id) {
+		
+		System.out.println(String.format("Id transférée depuis l'url %d", id));
+		
+		// Affiche page home pour le moment
+		return "home";
+	}
+	
+	@GetMapping("catalogue")
+	public String catalogue(@RequestParam("type") String type) {
+		
+		System.out.println(String.format("Id transférée depuis l'url %s", type));
+		
+		// Affiche page home pour le moment
+		return "home";
+	}
+	
+	@GetMapping("default-article")
+	public String defaultArticle(Model model) {
+		// crétation donée
+		String articleName = "Pizza";
+		
+		// Envoie de la donnée dans le model
+		model.addAttribute("articleName", articleName);
+		
+		return "article.html";
 	}
 }
