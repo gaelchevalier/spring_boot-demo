a9c7b17e8ec4e892cc9e73584c9b68941221dad6
gaelchevalier <89252419+gaelchevalier@users.noreply.github.com>
2023-05-26 16:51:34 +0200

    Demo Session - part2 + includes

--- src/main/java/fr/example/demo/controller/UserController.java
+++ src/main/java/fr/example/demo/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
 @SessionAttributes({"loggedUser"})
@@ -12,7 +13,7 @@ public class UserController {
 
     @GetMapping("/login")
     public String login(Model model){
-
+        //Stockage de la variable en sessoin
         model.addAttribute("loggedUser", new AppUser("test@test.fr", "test", "test"));
 
         return "home";
@@ -20,9 +21,23 @@ public class UserController {
 
     @GetMapping("/show-user")
     public String showLoggedUser(Model model){
-
+        // Récupération de la variable en session
         AppUser loggedUser = (AppUser) model.getAttribute("loggedUser");
-        System.out.println(loggedUser.getPseudo());
+        // Vérification de la présence de la variable
+        if (loggedUser == null) {
+            System.out.println("Vous êtes déconnectés");
+        } else {
+            System.out.println(String.format("vous êtes connecté : %s", loggedUser.getPseudo()));
+        }
         return "home";
+    }
+
+    @GetMapping("/logout")
+    public String logout(SessionStatus sessionStatus) {
+
+        // Vide la session
+        sessionStatus.setComplete();
+
+        return "redirect:/show-user";
     }
 }
